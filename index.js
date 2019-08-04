dec7f28de93205c37707e7ce64a2946e8f97e70b
s1lch1san <tutorialesamprazer@gmail.com>
2019-08-04 12:47:49 +0300

    removed token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = procces.env.token;
+const token = 'NjA3MjA0MzY5NDc3MTQwNDkx.XUajuw.hxl-NubfLWTx1v19zCjezeqzHjU';
 
 bot.on('ready', () =>{
     // 605164603986542592
